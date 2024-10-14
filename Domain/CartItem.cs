b5bb2ef299336037e3868108775a0f322643b95f
n1tuneyi <youssefhammam77@gmail.com>
2024-10-14 15:24:54 +0300

    create domain/entity objects

---
+++ Domain/CartItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Ecommerce.Domain;
+public class CartItem
+{
+    public long ProductId { get; set; }
+
+    public int Quantity { get; set; }
+
+    public decimal TotalPrice { get; set; }
+}
---
+++ Domain/ShoppingCart.cs
@@ -0,0 +1,9 @@
+﻿namespace Ecommerce.Domain;
+public class ShoppingCart
+{
+    public long Id { get; set; }
+
+    public long UserId { get; set; }
+
+    public List<CartItem> Items { get; set; }
+}
