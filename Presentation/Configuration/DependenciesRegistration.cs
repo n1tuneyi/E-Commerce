05be56f9a02b72f243fd034292e5348f3d6300db
n1tuneyi <youssefhammam77@gmail.com>
2024-10-30 18:11:26 +0300

    Move Dependency Registration to another Folder

---
+++ Presentation/Configuration/DependenciesRegistration.cs
@@ -0,0 +1,43 @@
+﻿using Application.Repositories;
+using Ecommerce;
+using Ecommerce.Domain.Repositories;
+using Ecommerce.Presenters;
+using Ecommerce.Services;
+using Infrastructure.Database;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Presentation.Configuration;
+
+public static class DependenciesRegistration
+{
+    public static IServiceCollection RegisterServices(this IServiceCollection services)
+    {
+        // Registering Services
+        services.AddScoped<AuthenticationService>();
+        services.AddScoped<CartService>();
+        services.AddScoped<ProductService>();
+        services.AddScoped<OrderService>();
+
+        // Registering Repositories
+        services.AddScoped<IProductRepository, ProductRepository>();
+        services.AddScoped<IOrderRepository, OrderRepository>();
+        services.AddScoped<IAuthRepository, UserRepository>();
+        services.AddScoped<ICartRepository, CartRepository>();
+
+        // Registering Presenters
+        services.AddScoped<AuthPresenter>();
+        services.AddScoped<UserPresenter>();
+        services.AddScoped<AdminPresenter>();
+        services.AddScoped<CartPresenter>();
+        services.AddScoped<OrderPresenter>();
+        services.AddScoped<ProductPresenter>();
+        services.AddScoped<StockPresenter>();
+        services.AddScoped<MainPresenter>();
+
+        // Registering Program Manager
+        services.AddScoped<ProgramManager>();
+        services.AddScoped<Database>();
+
+        return services;
+    }
+}
--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,9 +1,5 @@
-﻿using Application.Repositories;
-using Ecommerce.Domain.Repositories;
-using Ecommerce.Presenters;
-using Ecommerce.Services;
-using Infrastructure.Database;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Presentation.Configuration;
 
 namespace Ecommerce;
 
@@ -11,43 +7,13 @@ public class Program
 {
     static void Main(string[] args)
     {
-        var serviceCollection = new ServiceCollection();
-
-        RegisterServices(serviceCollection);
+        var serviceCollection = new ServiceCollection()
+                                    .RegisterServices();
 
         var serviceProvider = serviceCollection.BuildServiceProvider();
 
         var programManager = serviceProvider.GetService<ProgramManager>();
 
         programManager.Run();
-    }
-
-    private static void RegisterServices(IServiceCollection services)
-    {
-        // Registering Services
-        services.AddScoped<AuthenticationService>();
-        services.AddScoped<CartService>();
-        services.AddScoped<ProductService>();
-        services.AddScoped<OrderService>();
-
-        // Registering Repositories
-        services.AddScoped<IProductRepository, ProductRepository>();
-        services.AddScoped<IOrderRepository, OrderRepository>();
-        services.AddScoped<IAuthRepository, UserRepository>();
-        services.AddScoped<ICartRepository, CartRepository>();
-
-        // Registering Presenters
-        services.AddScoped<AuthPresenter>();
-        services.AddScoped<UserPresenter>();
-        services.AddScoped<AdminPresenter>();
-        services.AddScoped<CartPresenter>();
-        services.AddScoped<OrderPresenter>();
-        services.AddScoped<ProductPresenter>();
-        services.AddScoped<StockPresenter>();
-        services.AddScoped<MainPresenter>();
-
-        // Registering Program Manager
-        services.AddScoped<ProgramManager>();
-        services.AddScoped<Database>();
     }
 }
